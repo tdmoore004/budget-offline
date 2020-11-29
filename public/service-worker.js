8ff1d395ef3e77e3d94fab16db481940a99bd227
Tanner Moore <tdmoore004@gmail.com>
2020-11-28 21:37:28 -0700

    Added initial variables to service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,15 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+	"/index.html",
+	"/style.css",
+	"/db.js",
+	"/index.js",
+	"/manifest.webmanifest",
+	"/icons/icon-192x192.png",
+	"/icons/icon-512x512.png"
+];
+
 console.log("Hi from your service-worker.js file!");
