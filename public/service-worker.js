5a93dc321e84ef4818741d4619b2f0a9868cd419
Tanner Moore <tdmoore004@gmail.com>
2020-11-28 22:27:17 -0700

    Added activation code to service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,4 +22,22 @@ self.addEventListener("install", (event) => {
     self.skipWaiting();
 });
 
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      caches
+        .keys()
+        .then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).catch((error) => console.log("Error with Activiation: ", error))
+    );
+    self.clients.claim();
+  });
+
 console.log("Hi from your service-worker.js file!");
