43ed50247bece2e4679595cb1d8a10c0b6f00f58
Tanner Moore <tdmoore004@gmail.com>
2020-11-28 22:08:26 -0700

    Added install code to service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,12 +4,22 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
 	"/index.html",
-	"/style.css",
-	"/db.js",
+	"/styles.css",
+	// "/db.js",
 	"/index.js",
 	"/manifest.webmanifest",
 	"/icons/icon-192x192.png",
 	"/icons/icon-512x512.png"
 ];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .catch((error) => console.log("Error caching files: ", error))
+        );
+    self.skipWaiting();
+});
 
 console.log("Hi from your service-worker.js file!");
