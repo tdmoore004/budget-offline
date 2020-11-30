20d32b0c4d3406e2c3c4bbbea4751ab798ba9dc4
Tanner Moore <tdmoore004@gmail.com>
2020-11-29 20:44:57 -0700

    Added fetching code to service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,18 +15,18 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches
-            .open(CACHE_NAME)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-            .catch((error) => console.log("Error caching files: ", error))
+            .open(CACHE_NAME).then((cache) => {
+                cache.addAll(FILES_TO_CACHE)
+            }).catch((error) => {
+                console.log("Error caching files: ", error)
+            })
         );
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
     event.waitUntil(
-      caches
-        .keys()
-        .then(keyList => {
+      caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
@@ -35,9 +35,43 @@ self.addEventListener("activate", (event) => {
                     }
                 })
             );
-        }).catch((error) => console.log("Error with Activiation: ", error))
+        }).catch((error) => {
+            console.log("Error with Activiation: ", error)
+        })
     );
     self.clients.claim();
   });
+
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+          caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(event.request)
+              .then(response => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(event.request.url, response.clone());
+                }
+    
+                return response;
+              })
+              .catch(err => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(event.request);
+              });
+          }).catch(err => console.log(err))
+        );
+    
+        return;
+      }
+
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(event.request).then(response => {
+            return response || fetch(event.request);
+          });
+        })
+      );
+    });
 
 console.log("Hi from your service-worker.js file!");
