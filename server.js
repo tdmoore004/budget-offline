ce5b41a79556bf5b86b92b6d0139843956154ef8
Tanner Moore <tdmoore004@gmail.com>
2020-11-30 20:34:48 -0700

    Updated server.js for Heroku deployment with comments.

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
+// Require in necessary npm packages.
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// Setting up PORT.
+const PORT = process.env.PORT || 3000;
 
+// Setting up express to be used.
 const app = express();
 
+// Setting up morgan.
 app.use(logger("dev"));
 
+// Setting up middleware.
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Connecting to mongo database.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
